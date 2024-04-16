112b76b749640f2ce663d0d0cb4b8ea9e11a9836
Wulan Maulidya Putri Firmansyah <wulanmaulidya1604@gmail.com>
2024-04-16 19:50:55 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,126 @@
+<?php
+
+// use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\PageController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+// Basic routing
+// Route::get('/hello', function () {
+//     return 'Hello World';
+// });
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+// Route::get('/', function () {
+//     return 'Selamat Datang';
+// });
+
+// Route::get('/about', function () {
+//     return '2241720174 Wulan Maulidya P. F';
+// });
+
+// Route parameters
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya ' . $name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-' . $postId . " Komentar ke-: " . $commentId;
+});
+
+// Optional parameters
+Route::get('/user/{name?}', function ($name = 'John') {
+    return 'Nama saya ' . $name;
+});
+
+// route name
+Route::get('/user/profile', function () {
+    //
+})->name('profile');
+
+// Contoh penggunaan route group
+// Route::middleware(['first', 'second'])->group(function () { 
+//     Route::get('/', function () {
+//         // Uses first & second middleware...
+//     });
+
+//     Route::get('/user/profile', function () {
+//         // Uses first & second middleware...
+//     });
+// });    
+// Route::domain('{account}.example.com')->group(function () { 
+//     Route::get('user/{id}', function ($account, $id) {
+//         //
+//     });
+// });
+// Route::middleware('auth')->group(function () { 
+//     Route::get('/user', [UserController::class, 'index']); 
+//     Route::get('/post', [PostController::class, 'index']); 
+//     Route::get('/event', [EventController::class, 'index']);
+// });
+
+// Contoh penggunaan route prefixes
+// Route::prefix('admin')->group(function () { 
+//     Route::get('/user', [UserController::class, 'index']); 
+//     Route::get('/post', [PostController::class, 'index']); 
+//     Route::get('/event', [EventController::class, 'index']);
+
+// });
+
+// Contoh penggunaan redirect routes
+// Route::redirect('/here', '/there');
+
+// Contoh penggunaan view routes
+// Route::view('/welcome', 'welcome'); 
+// Route::view('/welcome', 'welcome', ['name' => 'Taylor']);
+
+// Membuat controller
+Route::get('/hello', [WelcomeController::class, 'hello']);
+// Route::get('/', [PageController::class, 'index']);
+// Route::get('/about', [PageController::class, 'about']);
+// Route::get('/articles/{id}', [PageController::class, 'articles']);
+
+// Modifikasi membuat controller
+// Route::get('/', [HomeController::class, 'index']);
+Route::get('/about', [AboutController::class, 'about']);
+Route::get('/articles/{id}', [ArticleController::class, 'articles']);
+
+// Resource Controller
+use App\Http\Controllers\PhotoController;
+
+Route::resource('photos', PhotoController::class)->only(['index', 'show']);
+Route::resource('photos', PhotoController::class)->except(['create', 'store', 'update', 'destroy']);
+
+// Membuat View
+// Route::get('/greeting', function () {
+//     return view('hello', ['name' => 'Wulan']);
+// });
+
+// Modifikasi membuat view
+// Route::get('/greeting', function () {
+//     return view('blog.hello', ['name' => 'Wulan']);
+// });
+
+//Menampilkan View dari Controller
+Route::get('/greeting', [WelcomeController::class, 'greeting']);
